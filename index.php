c9823ef6012ad3860cc7aa7251973265478dbe1a
noumecha <noumechaivan@gmail.com>
2022-03-04 13:47:36 +0100

    tmc --> creation de la première méthode

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-/**
- *  fichier d'entrée 
- *  permettant d'empecher la navigation dans les 
- *  fichiers de repertoire 
- *  @author : the tmc 
- */
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>my_simple_api</title>
+    </head>
+
+    <body>
+        
+        <?php
+        
+            /**
+             *  fichier d'entrée 
+             *  permettant d'empecher la navigation dans les 
+             *  fichiers de repertoire 
+             *  @author : the tmc 
+             */
+
+            //echo "ma première api complete avec php ";
+
+        ?>
+
+        <h1 text-align="center">
+
+            Ma Première API REST en PHP
+
+        </h1>
+
+    </body>
+</html>
--- produits.php
+++ produits.php
@@ -5,3 +5,78 @@
  * PUT,GET,POST,DELETE
  *  @author : the tmc 
  */
+
+# fichier de connection à la bd : 
+    require_once 'login.php';
+
+    # test de connection : 
+
+    if ($con_sqli)
+        echo "well";
+    else
+        echo "bad";
+
+    # la méthode utiliser :
+
+    $request_method = $_SERVER["REQUEST_METHOD"];
+
+    # test ici 
+    
+    echo "bonjour";
+    echo $request_method;
+
+    # en fonction de l améthode : 
+
+    switch($request_method)
+    {
+        case 'GET':
+            if(!empty($_GET["id"]))
+            {
+                # recupération d'un seul id :
+                $id = intval($_GET["id"]);
+                getProducts($id);
+            } 
+            else 
+            {
+                # récupération de tout les prods : 
+                getProducts();
+            }
+            break;
+        default:
+            # requete invalide : 
+            header("HTTP/1.0 405 Method Not Allowed");
+            break;
+    }
+
+    # methode getproducts() : 
+
+    /**
+     * la fonction de notre api pour récuperer les produits
+     * 
+     * @author : the tmc
+     */
+
+    function getProducts()
+    {
+        global $con_sqli;
+        $query = "SELECT * From produit";
+        $response = array();
+        $result = $con_sqli->query($query);
+
+        try {
+
+            while($row = mysqli_fetch_array($result))
+            {
+                $response[] = $row;
+            }
+            header('Conten-Type: application/json');
+            echo json_encode($response, JSON_PRETTY_PRINT);
+
+        } catch (\Throwable $th) {
+            
+            echo "erreur de connection : ".$th->getMessage()." ".$th->getCode();
+
+        }
+
+
+    }

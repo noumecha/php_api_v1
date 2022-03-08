cc260e194cf27ae2e339e6f90da35c7862896d26
noumecha <noumechaivan@gmail.com>
2022-03-08 15:33:29 +0100

    tmc --> add new user in the db ++

---
+++ api_jwt/api/register.php
@@ -0,0 +1,61 @@
+<?php
+include_once './config/database.php';
+/**
+ * ce fichier contien le code pour creer un
+ * nouvel utilisateur dans la base de donnée
+ * @author : the tmc
+ */
+
+# configuration du header : 
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, x-Requested-with");
+
+# les variables : 
+
+$firstName = '';
+$lastName = '';
+$email = '';
+$password = '';
+$conn = null;
+
+# config des var necessaires : 
+
+$database = new DatabaseService();
+$conn = $database->getConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+$firstName = $data->first_name;
+$lastName = $data->last_name;
+$email = $data->email;
+$password = $data->password;
+
+$table_name = 'Users';
+
+# debut du code : 
+
+$query = "INSERT INTO ". $table_name . " SET first_name = :firstname, last_name = :lastname, email = :email, password = :password";
+$stmt = $conn->prepare($query);
+
+$stmt->bindParam(':firstname', $firstName);
+$stmt->bindParam(':lastname',$lastName);
+$stmt->bindParam(':email',$email);
+
+$password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+$stmt->bindParam(':password',$password_hash);
+
+if($stmt->execute())
+{
+    http_response_code(200);
+    echo json_encode(array("message"=>"L'utilisateur a bien été enregistré."));
+}
+else
+{
+    http_response_code(400);
+    echo json_encode(array("message"=>"Impossible d'enregistrer l'utilisateur"));
+}
---
+++ api_jwt/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-dev-1x',
+        'version' => 'dev-dev-1x',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3e5fc63bc8ac2ad7cbb90acab9cea707da8431cf',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-dev-1x',
+            'version' => 'dev-dev-1x',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3e5fc63bc8ac2ad7cbb90acab9cea707da8431cf',
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.0.0',
+            'version' => '6.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => '0541cba75ab108ef901985e68055a92646c73534',
+            'dev_requirement' => false,
+        ),
+    ),
+);

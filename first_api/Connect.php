d7e8ec632a219693545b03a9a58270aa7c47fced
noumecha <noumechaivan@gmail.com>
2022-03-08 13:15:30 +0100

    tmc --> new soft api :) +++fun

---
+++ first_api/Connect.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * ce fichier est une classe utiliser pour la connection à notre bdd 
+ * @author : the tmc
+ */
+
+   //$connect = new PDO("mysql:host=adminer.skr; dbname=tmc_test",'root','root');
+
+   use PDO;
+   
+   class Connect {
+
+      private $pdo;
+
+      # constructeur : 
+      public function Connect(){
+
+      }
+
+      private function con(){
+         $this->pdo = new PDO("mysql:host=adminer.skr; dbname=tmc_test", 'root','root');
+      }
+
+   }
